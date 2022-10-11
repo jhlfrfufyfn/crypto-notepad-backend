106a094fa167f8ae21d67cb2293fb4724cff1ba4
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2022-10-11 16:46:42 +0300

    feat: encryption of files storage added

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -1,9 +1,11 @@
 import { appDataSource } from "../app-data-source";
 import { Repository } from "typeorm";
 
+import crypto from 'crypto';
+import config from 'config';
 import fs from "fs/promises";
 import path from "path";
-import { v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { User } from "../users/user.entity";
 import { File } from "./file.entity";
 
@@ -19,28 +21,29 @@ class FileService {
 
     async getFilesOfUser(userId: string) {
         return (
-        await this.fileRepository.
-                    createQueryBuilder("file")
-                    .leftJoin("file.owner", "owner")
-                    .where("owner.id = :userId", { userId })
-                    .getMany()
+            await this.fileRepository.
+                createQueryBuilder("file")
+                .leftJoin("file.owner", "owner")
+                .where("owner.id = :userId", { userId })
+                .getMany()
         );
     }
 
     async getFile(userId: string, fileId: string) {
         return (
-        await this.fileRepository.
-                    createQueryBuilder("file")
-                    .leftJoin("file.owner", "owner")
-                    .where("owner.id = :userId", { userId })
-                    .andWhere("file.id = :fileId", { fileId })
-                    .getOne()
+            await this.fileRepository.
+                createQueryBuilder("file")
+                .leftJoin("file.owner", "owner")
+                .where("owner.id = :userId", { userId })
+                .andWhere("file.id = :fileId", { fileId })
+                .getOne()
         );
     }
 
 
     async getFileContent(file: File) {
-        const text = await fs.readFile(this.getPathToFile(file), "utf-8");
+        let text = await fs.readFile(this.getPathToFile(file), "utf-8");
+        text = await this.decrypt(text)
         return text;
     }
 
@@ -52,7 +55,7 @@ class FileService {
 
         const filePath = this.getPathToFile(file)
         try {
-            await fs.writeFile(filePath, "");
+            await this.updateFileContent(file, '');
         } catch (err) {
             if (err instanceof Error) {
                 console.error(err.message);
@@ -66,8 +69,24 @@ class FileService {
         return await this.fileRepository.save(file);
     }
 
+    async encrypt(text: string) {
+        let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(config.get('file.secret')), null);
+        let encrypted = cipher.update(text);
+        encrypted = Buffer.concat([encrypted, cipher.final()]);
+        return encrypted.toString('hex');
+    }
+
+    async decrypt(text: string) {
+        let encryptedText = Buffer.from(text, 'hex');
+        let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(config.get('file.secret')), null);
+        let decrypted = decipher.update(encryptedText);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        return decrypted.toString();
+    }
+
     async updateFileContent(file: File, text: string) {
         const filePath = this.getPathToFile(file);
+        text = await this.encrypt(text);
         await fs.writeFile(filePath, text, "utf-8");
     }
 
